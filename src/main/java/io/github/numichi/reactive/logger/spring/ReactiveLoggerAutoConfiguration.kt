b4f6cfdb0756438b50c2cfd75943fe40105df69f
Donát Csongor <csdonat1@gmail.com>
2022-09-24 19:16:32 +0200

    fix: auto-configuration priority

--- src/main/java/io/github/numichi/reactive/logger/spring/ReactiveLoggerAutoConfiguration.kt
+++ src/main/java/io/github/numichi/reactive/logger/spring/ReactiveLoggerAutoConfiguration.kt
@@ -1,11 +1,14 @@
 package io.github.numichi.reactive.logger.spring
 
 import io.github.numichi.reactive.logger.Configuration as RLConfig
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
 import javax.annotation.PostConstruct
 
 @Configuration
+@AutoConfigureOrder(value = Ordered.HIGHEST_PRECEDENCE)
 @EnableConfigurationProperties(value = [ReactiveLoggerProperties::class])
 open class ReactiveLoggerAutoConfiguration(private val properties: ReactiveLoggerProperties) {
 
