84ed0d875c1c01e9acd5faf1c0426ce8bdfc5d24
Donát Csongor <csdonat1@gmail.com>
2024-03-08 18:49:34 +0100

    remove: Managed Dependencies from Maven Central

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,5 @@
 rootProject.name = "reactive-logger"
 
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "1.9.22"
-        kotlin("kapt") version "1.9.22"
-        kotlin("plugin.spring") version "1.9.22"
-        id("org.springframework.boot") version "3.2.3"
-        id("io.spring.dependency-management") version "1.1.4"
-        id("org.jmailen.kotlinter") version "4.2.0" apply false
-    }
-}
-
 include("reactive-logger")
 include("reactive-logger-example-kotlin")
 include("reactive-logger-example-java")
